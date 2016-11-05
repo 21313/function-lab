a71522b72855e368be13e9c6e0cf37f61534fb1f
hammad <hammad@abc.com>
2016-11-05 16:31:58 +0500

    functioncodeing

---
+++ app.js
@@ -0,0 +1,58 @@
+// function cook(ingrd){
+//     alert("being "+ingrd);
+// }
+// cook("egg");
+// function calculation(num1,num2,num3){
+//     result=(num1+num2)*num3;
+//     alert(result);
+// }
+// calculation(2,3,2);
+// function calculation1(){
+//     var num1 = parseInt(prompt("Enter first value"));
+//     var num2 = parseInt(prompt("Enter second value"));
+//     var num3 = parseInt(prompt("Enter third value"));
+//     result = (num1 + num2)*num3;
+//     if(result/result != 1){//ishe isNaN ke function se bhi kr skte hain
+//         alert("something went wrong plz provide a correct Number");
+//     }
+//     else
+//     {
+//     alert(result);
+//     }
+// }
+// calculation1();
+
+// var ingrd1 = ['egg', 'salt', 'sugar'];
+// var ingrd2 = ['water', 'egg', 'milk'];
+// var ingrd3 = ['flour', 'sugar', 'milk'];
+// var ingrd4 = [];
+
+// function combineIngrd(ing1, ing2, ing3) {
+//     for (var i = 0; i < ing1.length; i++) {
+//         if (ingrd4.indexOf(ing1[i]) == -1) {
+//             ingrd4.push(ingrd1[i]);
+//         }
+
+//     }
+//     for (var i = 0; i < ing2.length; i++) {
+//         if (ingrd4.indexOf(ing2[i]) == -1) {
+//             ingrd4.push(ingrd2[i]);
+//         }
+
+//     }
+//     for (var i = 0; i < ing3.length; i++) {
+//         if (ingrd4.indexOf(ing3[i]) == -1) {
+//             ingrd4.push(ingrd3[i]);
+//         }
+
+//     }
+//     console.log(ingrd4);
+// }
+// combineIngrd(ingrd1, ingrd2, ingrd3);
+
+// function addNumber(number1,number2){
+//     result = number1 + number2;
+//     return result;
+// }
+// var addition = addNumber(2,3);
+// alert(addition);
